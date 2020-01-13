ad067a4a49efbbb61616482bacc261bd0bb8c02c
bsalehe <bajuna.salehe@gmail.com>
2020-01-13 13:40:12 +0000

    script used to prepare manifest file.

---
+++ Previous projects/Yasmin_qiime2/scripts/prepare_manifest.sh
@@ -0,0 +1,1 @@
+sed -e 's/^/\/home\/bajuna\/bioinf_projects\/Yasmin_qiime2\/data\//' /home/bajuna/bioinf_projects/Yasmin_qiime2/data/sample_names.txt > /home/bajuna/bioinf_projects/Yasmin_qiime2/data/manifest_1.csv
