73a2f164cd71577ef98e039feddc7995019a8170
bsalehe <bajuna.salehe@gmail.com>
2020-01-13 15:39:35 +0000

    updated folder pevious project

--- Previous projects/Yasmin_qiime2/scripts/prepare_manifest.sh
+++
@@ -1,1 +0,0 @@
-sed -e 's/^/\/home\/bajuna\/bioinf_projects\/Yasmin_qiime2\/data\//' /home/bajuna/bioinf_projects/Yasmin_qiime2/data/sample_names.txt > /home/bajuna/bioinf_projects/Yasmin_qiime2/data/manifest_1.csv
